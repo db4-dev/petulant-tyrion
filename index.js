b81af8756a977a2767b30deebf6c259ef2cb5088
db4-dev <db4-dev@users.noreply.github.com>
2015-01-30 13:32:34 +0100

    Created index.js

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ var speakeasy = require("speakeasy");
 
 console.log("Generate", speakeasy.generate_key({length: 20, google_auth_qr: true}));
 
-console.log("TOPT-Key:", speakeasy.time({key: 'OUUGCKJOHRXC4NSFEFQU43DXPE6EMTCS', encoding: 'base32'}));
+var timing = [-60, -30, 0, 30, 60];
+
+for (var i = 0; i < timing.length; i++) {
+	console.log("TOPT-Key:", timimg[i], speakeasy.time({key: 'OUUGCKJOHRXC4NSFEFQU43DXPE6EMTCS', encoding: 'base32', initial_time: timing[i]}));
+}
